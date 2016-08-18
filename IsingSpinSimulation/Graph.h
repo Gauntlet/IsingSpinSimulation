0cf66c97a2da0fa639ff03f845134ce9b6537907
Gauntlet <gauntlet.develop@outlook.com>
2016-08-18 18:07:24 +0100

    Added type enum to graph class

--- IsingSpinSimulation/Graph.h
+++ IsingSpinSimulation/Graph.h
@@ -1,31 +1,62 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 #include "DataStructures.h"
-namespace kspace {
-	class Graph {
+namespace kspace
+{
+	class Graph
+	{
+	private:
+
 	public:
-		static class Parameters {
+		enum class Type 
+		{
+			Rectangular_Lattice, Circular_Lattice, Erdos_Renyi, Watts_Strogatz, Barabasi_Albert
+		};
+
+		struct Parameters
+		{
+
+		} parameters;
+
+		static class Generator
+		{
+			static class Parameters
+			{
 			public:
-			static struct Rectangular_Lattice {
-				size_t W;
-				size_t H;
-			};
+				static struct Rectangular_Lattice
+				{
+					size_t W;
+					size_t H;
+				};
 
-			static struct Circular_Lattice {
-				size_t N;
-				size_t K;
-			};
+				static struct Circular_Lattice
+				{
+					size_t N;
+					size_t K;
+				};
 
-			static struct Erdos_Renyi {
-				size_t N;
-				double P;
-				size_t seed;
-			};
+				static struct Erdos_Renyi
+				{
+					size_t N;
+					double P;
+					size_t seed;
+				};
 
-			static struct Watts_Strogatz {
-				size_t N;
-				size_t K;
-				double P;
+				static struct Watts_Strogatz
+				{
+					size_t N;
+					size_t K;
+					double P;
+					size_t seed;
+				};
+
+				static struct Barabasi_Albert
+				{
+					size_t N;
+					size_t M;
+					size_t K;
+					size_t seed;
+				};
 			};
 		};
 	};
